e5b8fe23988249d8cd66b978f6e67e8c6ceb771c
Carl Parker <Carl.Parker@hbo.com>
2015-05-19 14:30:12 -0500

    able to bring up take-photo UI and dismiss

---
+++ image-picker-uw-ios-hw5/ImageListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageListTableViewController.h
+//  image-picker-uw-ios-hw5
+//
+//  Created by Parker, Carl (HBO) on 5/19/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageListTableViewController : UITableViewController
+
+@end
